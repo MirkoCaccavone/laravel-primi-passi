0904a047ad3714108d3ce3410fa96a723f7966ae
Mirko <mirkocaccavone03@gmail.com>
2025-05-09 17:40:58 +0200

    [feat] Add CSS styles and define routes for Home, About, and Contacts pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home', ['titolo' => 'Benvenuto nella Home']);
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about', ['titolo' => 'Chi siamo']);
+})->name('about');
+
+Route::get('/contacts', function () {
+    return view('contacts', ['titolo' => 'Contattaci']);
+})->name('contacts');
