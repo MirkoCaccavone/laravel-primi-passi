7588da19e7701287b6d04adff7e4ea6e01f76f31
Mirko <mirkocaccavone03@gmail.com>
2025-05-09 17:40:08 +0200

    [feat] Add initial views for About, Contacts, and Home pages

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <title>contacts</title>
+</head>
+
+<body>
+    @include('partials.header')
+
+    <h1>{{$titolo}}</h1>
+</body>
+
+</html>
